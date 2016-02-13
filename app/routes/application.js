9fe05bc0ced4122e285382306f1fdc10d4621002
jmccune <justin.mccune@gmail.com>
2016-02-13 16:43:01 -0500

    test DxrefCore integration (without errors).

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 import EmberLog4jsLogger from 'ember-log4js/log4js-basic';
+import FooObject from 'tap/core/errors';
 
 var logger = EmberLog4jsLogger.getLogger('test-addon-app.initialization');
 
 function appInit() {
   console.log("Tap Initialization Starting...");
   logger.warn("THIS IS A TEST MESSAGE ONLY!");
+  setTimeout(function() {
+    console.dir(FooObject);
+    //throw new DxrefError('application', 'appInit', 'this is a test messagae that regnerates every 5 seconds');
+
+  }, 5000);
   console.log("Tap Initialization COMPLETE!");
 }
 
